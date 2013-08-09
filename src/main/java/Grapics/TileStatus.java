0a0a52c9aae98241ffdbbcf762c86c16ddac623b
Shiera <cecilia.aijala@suniplan.fi>
2013-08-09 15:39:46 +0300

    huomasin ettei luokkakaavio ollutkaan menny gittiin, tiistaina tehty sellainen tässä nyt

---
+++ src/main/java/Grapics/TileStatus.java
@@ -0,0 +1,11 @@
+package Grapics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cec
+ * Date: 8/9/13
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum TileStatus {
+}
