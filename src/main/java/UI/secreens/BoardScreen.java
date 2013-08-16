eba5945edee1aacdce178e8b31fa59d70c59454f
Shiera <cecilia.aijala@suniplan.fi>
2013-08-16 18:29:16 +0300

    added mouseTracker on board

--- src/main/java/UI/secreens/BoardScreen.java
+++ src/main/java/UI/secreens/BoardScreen.java
@@ -6,6 +6,7 @@ package UI.secreens;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import javax.swing.*;
 
 import UI.*;
@@ -29,6 +30,10 @@ public class BoardScreen extends JPanel{
     private int xBoardOrigoCord = 32;
     private int yBoardOrigoCord = 32;
 
+    private int choosedTileX = -1;
+    private int choosedTileY = -1;
+    private Picture choosedTile = new Picture("choosedTile.png");
+
 
 
     private UI.Button checkButton;
@@ -36,65 +41,76 @@ public class BoardScreen extends JPanel{
 
 
     public BoardScreen(final GameLogic game, BaseFrame frame) {
-        System.out.println("start");
 
         this.game = game;
 
-
-
         makeButtons(frame);
-        addMouseListener(game);
+        addMouse(game);
 
     }
 
-    private void addMouseListener(final GameLogic game) {
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int posX = e.getX();
-                int posY = e.getY();
-                int tileX = (posX - xBoardOrigoCord) / tileSize;
-                int tileY = (posY - yBoardOrigoCord) / tileSize;
-                if (posX < xBoardOrigoCord || posY < yBoardOrigoCord) {
-                    tileX = -1;
-                    tileY = -1;
+    /**
+     * do when mouse was clicked
+     * @param e
+     * @param game
+     */
+    private void whenClicked(MouseEvent e, GameLogic game) {
+        int posX = e.getX();
+        int posY = e.getY();
+        int tileX = (posX - xBoardOrigoCord) / tileSize;
+        int tileY = (posY - yBoardOrigoCord) / tileSize;
+        if (posX < xBoardOrigoCord || posY < yBoardOrigoCord) {
+            tileX = -1;
+            tileY = -1;
 
-                }
+        }
+        int mouseButton = e.getButton();
+        // frame päättää paneelien välillä vaihtamisen
+        if (mouseButton == LEFTBUTTON) {
+            game.doMove(tileX, tileY, UNCOVERED);
+        }
+        if (mouseButton == RIGHTBUTTON) {
+            if (game.getBoard().getStatusXY(tileX, tileY) == MARKED) {
+                game.doMove(tileX, tileY, COVERED);
+            } else game.doMove(tileX, tileY, MARKED);
+        }
 
+        // jos painettiin valmisnappia
+        if (mouseButton == LEFTBUTTON && game.flagsLeft() == 0) {
+            checkButton.ifClicked(posX, posY);
+        }
 
-                int mouseButton = e.getButton();
-
-
-                // frame päättää paneelien välillä vaihtamisen
-                if (mouseButton == LEFTBUTTON) {
-                    game.doMove(tileX, tileY, UNCOVERED);
-                }
-                if (mouseButton == RIGHTBUTTON) {
-                    if (game.getBoard().getStatusXY(tileX, tileY) == MARKED) {
-                        game.doMove(tileX, tileY, COVERED);
-                    } else game.doMove(tileX, tileY, MARKED);
-                }
-
-                // jos painettiin valmisnappia
-                if (mouseButton == LEFTBUTTON && game.flagsLeft() == 0) {
-                    checkButton.ifClicked(posX, posY);
-                }
-
-                menuButton.ifClicked(posX, posY);
+        menuButton.ifClicked(posX, posY);
 
 
-                if (!game.isPlaying()) {
-                    if (game.isHasWon()) {
-                        // TODO print winscreen
-                    } else {
-                        // TODO game is lost
-                        lost = true;
-                    }
-                }
-                repaint();
-
+        if (!game.isPlaying()) {
+            if (game.isHasWon()) {
+                // TODO print winscreen
+            } else {
+                // TODO game is lost
+                lost = true;
             }
-        });
+        }
+    }
+
+    /**
+     * do when mouse was mooved
+     * @param e
+     * @param game
+     */
+    private void mouseMoove(MouseEvent e, GameLogic game) {
+        choosedTileX = (e.getX() - xBoardOrigoCord) / tileSize;
+        choosedTileY = (e.getY() - yBoardOrigoCord) / tileSize;
+        if (    e.getX() < xBoardOrigoCord ||
+                e.getY() < yBoardOrigoCord ||
+                choosedTileX > game.getBoard().getBoardSize()-1 ||
+                choosedTileY> game.getBoard().getBoardSize()-1) {
+
+            choosedTileX = -1;
+            choosedTileY = -1;
+
+        }
+
     }
 
 
@@ -114,20 +130,24 @@ public class BoardScreen extends JPanel{
                 game.getBoard().getTileAppearance(x, y).drawImage(x,y,g2, tileSize, xBoardOrigoCord, yBoardOrigoCord);
             }
         }
-        g2.drawString("Flags left: " + game.flagsLeft() , lastCordinateOfBoardX/2 , lastCordinateOfBoardY + tileSize);
+        g2.drawString("Flags left: " + game.flagsLeft(), lastCordinateOfBoardX / 2, lastCordinateOfBoardY + tileSize);
         // draw if flags used
         if (  game.flagsLeft() == 0){
             g2.drawString("No flags left to put, press button to check" , tileSize , lastCordinateOfBoardY + (2*tileSize));
             checkButton.draw(g2);
         }
         menuButton.draw(g2);
+        if (choosedTileY != -1){
+            choosedTile.draw(g2, (choosedTileX*tileSize)+xBoardOrigoCord, (choosedTileY*tileSize)+yBoardOrigoCord);
+        }
 
     }
 
 
-
-
-
+    /**
+     * make all the buttons in the gameScreen (not the tiles)
+     * @param frame
+     */
     private void makeButtons(BaseFrame frame){
         checkButton = new UI.Button( frame,"grass.jpg", game.getBoard().getBoardSize()/2*tileSize, game.getBoard().getBoardSize()*tileSize+ 2*tileSize, new ButtonHandler() {
             @Override
@@ -142,5 +162,31 @@ public class BoardScreen extends JPanel{
             }
         });
     }
+
+
+    private void addMouse(final GameLogic game) {
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                mouseMoove(e, game);
+                repaint();
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                whenClicked(e, game);
+                repaint();
+
+            }
+        });
+    }
+
 
 }
